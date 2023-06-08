43426f11293305799a6a985099391a918d0907e4
MarioMonts <mario.montes@uniat.edu.mx>
2023-06-08 10:49:43 -0600

    First Commit

--- TikTakToe.cpp
+++ TikTakToe.cpp
@@ -1,15 +1,15 @@
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <clocale>
 
-
-
-
-
+using namespace std;
 
 int main()
 {
-
-
 
 
     std::cout << "Hello World!\n";
